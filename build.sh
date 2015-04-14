5571d0f9baf7addcb1b16441e2e3714615d3f0d1
xzyfer <xzyfer@gmail.com>
2015-04-14 18:29:35 +1000

    Add image for Libsass 3.1.0

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# build
+apk --update add git build-base
+git clone https://github.com/sass/sassc
+cd sassc
+git checkout 3.1.0
+git clone https://github.com/sass/libsass
+cd libsass
+git checkout 3.1.0
+cd ..
+SASS_LIBSASS_PATH=/sassc/libsass make
+
+# install
+mv bin/sassc /usr/bin/sassc
+
+# cleanup
+cd /
+rm -rf /sassc
+apk del git build-base
+apk add libstdc++
+rm -rf /var/cache/apk/*
